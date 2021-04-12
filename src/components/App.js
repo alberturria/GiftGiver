5345a104cbf1f955764d5e9eeaad059c1cc6b4ac
Alberto Herrera Vargas <albertoherreravargas@gmail.com>
2021-04-12 21:12:14 +0200

    Adding new add gift feature `add gift to the list` by applying TDD

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import GiftEntity from '../entities/GiftEntity';
 
 export default class App extends Component {
     constructor() {
@@ -6,10 +8,31 @@ export default class App extends Component {
 
         this.state = { gifts: [] }
     }
+
+    addGift = () => {
+        const { gifts } = this.state;
+        const existingIds = gifts.map( gift =>  gift.id);
+        const maxId = existingIds.length > 0 ? Math.max(...existingIds) + 1 : 0;
+        const newGift = new GiftEntity(maxId)
+        gifts.push(newGift);
+
+        this.setState({ gifts });
+    }
+
+    _renderGifts = () => {
+        const { gifts } = this.state;
+
+        return gifts.map(gift => <div key={gift.id} />);
+    }
+
     render() {
         return (
             <div>
                 <h2>Gift Giver</h2>
+                <div className='gifts-list'>
+                    {this._renderGifts()}
+                </div>
+                <Button className='button-add-gift' onClick={this.addGift}>Add Gift</Button>
             </div>
         )
     }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import GiftEntity from '../entities/GiftEntity';
 
 const app = shallow(<App />);
 
@@ -11,4 +12,23 @@ it('should render the react root node', () => {
 
 it('Should initialize the `state` as am empty list of gifts', () => {
     expect(app.state().gifts).toEqual([]);
+})
+
+it('Should add a new Gift to the `state` when clicking on the `add gift` button', () => {
+    const expectedGiftEntity = new GiftEntity(0);
+    const addGiftButton = app.find('.button-add-gift');
+    addGiftButton.simulate('click');
+    const stateGifts = app.state().gifts;
+    
+    expect(stateGifts).toEqual([expectedGiftEntity]);
+})
+
+it('Should add a new Gift to the gifts list when clickin on the add Gift Button', () => {
+    const expectedNumberOfGifts = 1;
+    const addGiftButton = app.find('.button-add-gift');
+    addGiftButton.simulate('click');
+    const giftList = app.find('.gifts-list');
+    const numberOfGifts = giftList.children.length;
+    
+    expect(numberOfGifts).toEqual(expectedNumberOfGifts);
 })
